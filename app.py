5b2fd06fd9a972f08f6d715c2b1a664b76b66143
Maaz Mansoori <maazfootball@gmail.com>
2026-06-30 17:59:26 +0530

    Render deployment update

--- app.py
+++ app.py
@@ -890,6 +890,10 @@ def qr_generator():
 # MAIN
 # -----------------------
 
+import os
+
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
